dc0fcad074b15016f54e84315b19453aec2fafdc
Nick Allen <rutherford.t.b@gmail.com>
2023-09-15 00:44:11 -0500

    add template

---
+++ templates/solution.test.ts
@@ -0,0 +1,14 @@
+/*
+import { describe, test, expect } from "bun:test";
+import solution from "./solution";
+
+const cases: Array<[string, number]> = [];
+
+describe("cases", () => {
+    for (let i = 0; i < cases.length; ++i) {
+        test(`${cases[i][0]} => ${cases[i][1]}`, () => {
+            expect(solution(cases[i][0])).toEqual(cases[i][1]);
+        });
+    }
+});
+*/
